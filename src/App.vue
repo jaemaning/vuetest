d103abb0f3e5123e46c326739e897b478512e605
김재만 <lovesay00@naver.com>
2022-12-01 03:45:52 +0900

    20221201_kjm(vue methods, v-for, data)

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,34 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="menu">
+    <a v-for="menu in menus" :key="menu">{{menu}}</a>
+  </div>
+
+  <div v-for="(product,index) in products" :key="index" class = 'card'>
+    <h4>{{product.product}}</h4>
+    <p>{{product.price}}만원</p>
+    <button v-on:click="increase(index)" class="red-btn">허위매물신고</button> <span>신고수 : {{reportNum[index]}}</span>
+  </div>
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
 
 export default {
   name: 'App',
+  data() {
+    return {
+      menus : ['Home','Products','About'],
+      products : [{product : '역삼동원룸', price: 60, id : 0}, {product : '천호동원룸', price : 40, id : 1}, {product : '마포구원룸', price : 70, id :2}],
+      reportNum : [0,0,0]
+    }
+  },  // data 보관함 ( 데이터 다 떄려 박기 ) vue 실시간 자동 렌더링을 위해 데이터 바인딩이 필요
+  methods: {
+    increase(index) {
+      this.reportNum[index]++
+    }
+  },
   components: {
-    HelloWorld
   }
 }
 </script>
@@ -21,6 +40,25 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+.menu {
+  background: darkorchid;
+  padding:15px;
+  border-radius: 5px;
+}
+
+.menu a{
+  color : white;
+  padding : 10px;
+}
+
+.red-btn {
+  color : white;
+  background-color: darkred;
+  border-radius: 5px;
+  border : 0px;
+  height : 40px;
+  width : 130px
 }
 </style>
